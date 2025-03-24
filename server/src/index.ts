30776ce6822b84a1a2a82ea4b8f96d9bb77f29f9
Kirtan Kakadiya <35823164+KirtanKakadiya@users.noreply.github.com>
2025-03-24 12:32:04 -0600

    added websocket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,11 @@ import {
   authenticateSocketToken,
   authenticateToken,
 } from "./middleware/authMiddleware";
+import { CronJob } from 'cron';
 
+
+import {electionMsg, leaderMsg, healthMsg, manageDatabaseCluster} from "./leader-election/leaderElection"
+import { WebSocket, WebSocketServer } from "ws";
 dotenv.config();
 
 const app = express();
@@ -23,6 +27,7 @@ const corsOptions = {
 };
 
 export const dbClient = createDbClient(process.env.DATABASE_URL ?? "");
+export const replicaDbClient = createDbClient(process.env.REPICA_DATABASE_URL ?? "");
 
 app.use(cors(corsOptions));
 
@@ -43,6 +48,29 @@ app.get("/health", (_req, res) => {
 
 const server = app.listen(port, () => {
   console.log(`🐢 Server running at: http://localhost:${port}`);
+});
+const wsServer = new WebSocket.Server({ server: server });
+
+wsServer.on("connection", (ws: WebSocket) => {
+  // console.log('New connection!');
+  ws.on("message", async (message: string) => {
+      const data = JSON.parse(message);
+      // console.log(data)
+      if (data.type === "election") {
+        electionMsg(data.port, ws);
+      }
+
+      if (data.type === "leader") {
+        leaderMsg(data.leader, ws);
+      }
+
+      if (data.type === "health") {
+        healthMsg(ws);
+      }
+      ws.close();
+  });
+
+  ws.on("close", () => console.log("Client disconnected"));
 });
 
 export const io = new Server(server, {
@@ -69,3 +97,40 @@ io.on("connection", (socket: AuthenticatedSocket) => {
     console.log("Client Disconnected with ID: ", userId);
   });
 });
+
+
+wsServer.on("connection", (ws: WebSocket) => {
+  ws.on("message", async (message: string) => {
+      const data = JSON.parse(message);
+      console.log(data)
+      if (data.type === "election") {
+        electionMsg(data.port, ws);
+      }
+
+      if (data.type === "leader") {
+        leaderMsg(data.leader, ws);
+      }
+
+      if (data.type === "health") {
+        healthMsg(ws);
+      }
+  });
+});
+
+const job = new CronJob(
+	'* * * * *', // cronTime every minute
+  async () => { // Wrap manageDatabaseCluster in an async function
+    await manageDatabaseCluster();
+  }, // onTick
+	null, // onComplete
+	true, // start
+	'America/Los_Angeles' // timeZone
+);
+(async () => {
+  try {
+    console.log("🔄 Running manageDatabaseCluster on cold start...");
+    await manageDatabaseCluster();
+  } catch (error) {
+    console.error("❌ Error running manageDatabaseCluster on startup:", error);
+  }
+})();
