d7c14241b5327cab3b844c9523cfa3796e8c1e20
Sajan Hayer <sajanhayer@Sajans-MacBook-Pro.local>
2025-04-03 23:47:43 -0600

    Added updated drizzle script

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,12 +1,31 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import { defineConfig } from "drizzle-kit";
+
+// export default defineConfig({
+//   out: "./drizzle",
+//   schema: "./src/db/schema.ts",
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     url: process.env.DEV_DATABASE_URL ?? "",
+//   },
+//   verbose: true,
+//   strict: true,
+// });
+
+
+import * as dotenv from 'dotenv';
+dotenv.config();  // Load the environment variables
+
 import { defineConfig } from "drizzle-kit";
+
+const dbUrl = process.env.DEV_DATABASE_URL || process.env.DEV_DATABASE_URL2
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DEV_DATABASE_URL ?? "",
+    url: dbUrl ?? "",  // Default to an empty string if no URL is set
   },
   verbose: true,
   strict: true,
