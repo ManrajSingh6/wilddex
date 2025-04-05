e61b2db99d61e854abfa5f53b792a644c51a82be
Manraj Singh <manrajs2025@gmail.com>
2025-04-04 21:25:08 -0600

    Remove commented code, minor tweak to package script

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,31 +1,19 @@
-// import "dotenv/config";
-// import { defineConfig } from "drizzle-kit";
-
-// export default defineConfig({
-//   out: "./drizzle",
-//   schema: "./src/db/schema.ts",
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: process.env.DEV_DATABASE_URL ?? "",
-//   },
-//   verbose: true,
-//   strict: true,
-// });
-
-
-import * as dotenv from 'dotenv';
-dotenv.config();  // Load the environment variables
+import * as dotenv from "dotenv";
+dotenv.config(); // Load the environment variables
 
 import { defineConfig } from "drizzle-kit";
 
-const dbUrl = process.env.DEV_DATABASE_URL || process.env.DEV_DATABASE_URL2
+const dbUrl =
+  process.env.DEV_DATABASE_URL ||
+  process.env.DEV_DATABASE_URL2 ||
+  process.env.DEV_DATABASE_URL3;
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: dbUrl ?? "",  // Default to an empty string if no URL is set
+    url: dbUrl ?? "", // Default to an empty string if no URL is set
   },
   verbose: true,
   strict: true,
