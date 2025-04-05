fec1f4426b83a460a3ab52d227949044fd7369cc
Manraj Singh <manrajs2025@gmail.com>
2025-04-04 21:38:22 -0600

    LETS GOOO ALL MIGRATIONS WOKRING ON REPLICA DBs

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,40 +1,45 @@
-import { Pool } from "pg"
-import { drizzle } from "drizzle-orm/node-postgres"
-import { migrate } from "drizzle-orm/node-postgres/migrator"
+import dotenv from "dotenv";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+
+dotenv.config();
 
 const DB_CLIENT_URLS = [
-    process.env.DATABASE_URL,//postgres:wilddex@db-primary:5432/wilddex-primary-db
-    process.env.REPLICA_DATABASE_URL,//postgres:wilddex@db-replica:5433/wilddex-replica-db
-    process.env.REPLICA2_DATABASE_URL//postgres:wilddex@db-replica2:5434/wilddex-replica2-db
-].filter((url) => url !== undefined)
+  process.env.DEV_DATABASE_URL,
+  process.env.DEV_DATABASE_URL2,
+  process.env.DEV_DATABASE_URL3,
+].filter((url) => url !== undefined);
 
 async function migrateDatabases(): Promise<void> {
-    for (const url of DB_CLIENT_URLS) {
-        try {
-            const pool = new Pool({
-                connectionString: url
-            })
+  for (const url of DB_CLIENT_URLS) {
+    try {
+      const pool = new Pool({
+        connectionString: url,
+      });
 
-            const db = drizzle(pool)
+      const db = drizzle(pool);
 
-            await migrate(db, {
-                migrationsFolder: '../../drizzle'
-            })
+      await migrate(db, {
+        migrationsFolder: "drizzle",
+      });
 
-            console.log(`Finished migrating ${url}`)
-
-        } catch (error) {
-            console.error(`Error migrating to DB (${url}): error=${JSON.stringify(error)}`)
-            return
-        }
+      console.log(`Finished migrating ${url}`);
+    } catch (error) {
+      console.error(error);
+      console.error(
+        `Error migrating to DB (${url}): error=${JSON.stringify(error)}`
+      );
+      return;
     }
+  }
 }
 
 migrateDatabases()
   .then(() => {
-    console.log('🏁 All migrations complete');
+    console.log("🏁 All migrations complete");
     process.exit(0);
   })
   .catch((err) => {
-    console.error('❌ Migration process failed:', err);
+    console.error("❌ Migration process failed:", err);
   });
